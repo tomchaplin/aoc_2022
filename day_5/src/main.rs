c1a6546aafc1ac4595494a4efee7aa947ee7b1d0
Thomas Chaplin <tomrchaplin@gmail.com>
2022-12-09 12:10:35 +0000

    Refactoring

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -2,34 +2,13 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 #[derive(Debug, Clone, Copy)]
-struct Job {
-    from: usize,
-    to: usize,
-}
-
-#[derive(Debug, Clone, Copy)]
 struct BatchJob {
     count: usize,
     from: usize,
     to: usize,
 }
 
-// TODO: Convert from batch to iterator of jobs
-
-fn parse_line(line: Vec<String>) -> Vec<Job> {
-    let count: usize = line[1].parse().expect("Bad number of moves");
-    let from: usize = line[3].parse().expect("Bad from bin");
-    let to: usize = line[5].parse().expect("Bad to bin");
-    vec![
-        Job {
-            from: from - 1,
-            to: to - 1
-        };
-        count
-    ]
-}
-
-fn parse_line_as_batch(line: Vec<String>) -> BatchJob {
+fn parse_line(line: Vec<String>) -> BatchJob {
     let count: usize = line[1].parse().expect("Bad number of moves");
     let from: usize = line[3].parse().expect("Bad from bin");
     let to: usize = line[5].parse().expect("Bad to bin");
@@ -40,28 +19,30 @@ fn parse_line_as_batch(line: Vec<String>) -> BatchJob {
     }
 }
 
-fn read_lines() -> impl Iterator<Item = Job> {
+fn read_lines() -> impl Iterator<Item = BatchJob> {
     let file = File::open("./data/input").expect("Input file not found");
     let lines = io::BufReader::new(file).lines();
     lines
         .map(|line| line.expect("Couldn't read line of input"))
         .map(move |line| line.split(" ").map(|str| String::from(str)).collect())
         .filter(|line: &Vec<String>| line[0] == "move")
-        .map(|line| {
-            let job_vec = parse_line(line);
-            return job_vec.into_iter();
-        })
-        .flatten()
+        .map(parse_line)
 }
 
-fn read_lines_as_batch() -> impl Iterator<Item = BatchJob> {
-    let file = File::open("./data/input").expect("Input file not found");
-    let lines = io::BufReader::new(file).lines();
-    lines
-        .map(|line| line.expect("Couldn't read line of input"))
-        .map(move |line| line.split(" ").map(|str| String::from(str)).collect())
-        .filter(|line: &Vec<String>| line[0] == "move")
-        .map(parse_line_as_batch)
+fn split_jobs(batch_jobs: impl Iterator<Item = BatchJob>) -> impl Iterator<Item = BatchJob> {
+    batch_jobs
+        .map(|batch_job| {
+            vec![
+                BatchJob {
+                    from: batch_job.from,
+                    to: batch_job.to,
+                    count: 1
+                };
+                batch_job.count
+            ]
+            .into_iter()
+        })
+        .flatten()
 }
 
 fn do_batch_job(mut state: Vec<Vec<char>>, job: BatchJob) -> Vec<Vec<char>> {
@@ -70,12 +51,6 @@ fn do_batch_job(mut state: Vec<Vec<char>>, job: BatchJob) -> Vec<Vec<char>> {
     let mut split_elements = state[job.from].split_off(split_point);
     state[job.to].append(&mut split_elements);
     state
-}
-
-fn do_job(mut state: Vec<Vec<char>>, job: Job) -> Vec<Vec<char>> {
-    let popped_element = state[job.from].pop().expect("Popped to many");
-    state[job.to].push(popped_element);
-    return state;
 }
 
 fn get_final_letters(state: Vec<Vec<char>>) -> String {
@@ -98,12 +73,13 @@ fn main() {
         vec!['L', 'S', 'G'],
     ];
 
-    let jobs = read_lines();
-    let final_state_a = jobs.fold(init_state.clone(), do_job);
+    let batch_jobs = read_lines();
+    let jobs = split_jobs(batch_jobs);
+    let final_state_a = jobs.fold(init_state.clone(), do_batch_job);
     let final_letters_a = get_final_letters(final_state_a);
     println!("{}", final_letters_a);
 
-    let batch_jobs = read_lines_as_batch();
+    let batch_jobs = read_lines();
     let final_state_b = batch_jobs.fold(init_state, do_batch_job);
     let final_letters_b = get_final_letters(final_state_b);
     println!("{}", final_letters_b);
